35725fdcd38ea72d58457904624f80470b104092
Jeff Heuer <jheuer@gmail.com>
2009-11-12 16:01:55 -0500

    use json formatting

--- app/controllers/data_snippets_controller.rb
+++ app/controllers/data_snippets_controller.rb
@@ -20,7 +20,7 @@ class DataSnippetsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @data_snippet }
-      format.json { render :json => @data_snippet.to_json }
+      format.json { render :json => @data_snippet.body.to_json }
     end
   end
 
